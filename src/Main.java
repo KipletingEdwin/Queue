5ba210a2d5783b6d5647c5f070bb9921405517a4
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-05-11 20:54:13 +0300

    add three strings to Queue

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Queue<String> bbqLine = new LinkedList<String>();
+        bbqLine.add("Jackson");
+        bbqLine.add("Tyreek");
+        bbqLine.add("Susan");
+
+        System.out.println(bbqLine);
+
     }
 }
