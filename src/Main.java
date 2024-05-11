82e934f7995494fb51e9b35e3ce75e11878701ce
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-05-11 21:11:03 +0300

    finalie on Queue

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,16 @@ public class Main {
         bbqLine.add("Jackson");
         bbqLine.add("Tyreek");
         bbqLine.add("Susan");
+        bbqLine.add("Ken");
+        bbqLine.add("Edgar");
+
+        bbqLine.poll();
+
+        System.out.println(bbqLine.peek());
+        System.out.println(bbqLine.size());
+        System.out.println(bbqLine.contains("Jackson"));
+        System.out.println(bbqLine.toArray()[2]);
+
 
         System.out.println(bbqLine);
 
